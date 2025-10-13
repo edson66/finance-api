5029d803877611843372066f62230613749a95ba
Edson Ulisses <edsonulissesr1@gmail.com>
2025-10-13 01:10:26 -0300

    sumário filtrado por DATA

--- src/main/java/com/finance/api/controllers/SumarioController.java
+++ src/main/java/com/finance/api/controllers/SumarioController.java
@@ -5,11 +5,17 @@ import com.finance.api.domain.dadosTransacoes.TransacaoRepository;
 import com.finance.api.domain.dadosTransacoes.TransacaoService;
 import com.finance.api.domain.dadosUsuario.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @RestController
 @RequestMapping("/sumario")
@@ -27,6 +33,24 @@ public class SumarioController {
 
         var sumario = service.gerarSumario(transacoes);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok().body(sumario);
+    }
+
+    @GetMapping("por-data")
+    public ResponseEntity gerarSumarioPorData(@AuthenticationPrincipal Usuario usuario,
+                                              @RequestParam("dataInicio") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataInicio,
+                                              @RequestParam("dataFinal") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataFinal){
+
+        if (dataInicio.isAfter(dataFinal)) {
+            return ResponseEntity.badRequest().body("A data de início não pode ser posterior à data final.");
+        }
+
+        var dataInicioHorario = dataInicio.atStartOfDay();
+        var dataFinalHorario = dataFinal.atTime(LocalTime.MAX);
+
+        var transacoes = repository.findByUsuarioAndDataBetween(usuario, dataInicioHorario, dataFinalHorario);
+        var sumario = service.gerarSumario(transacoes);
+
+        return ResponseEntity.ok(sumario);
     }
 }
--- src/main/java/com/finance/api/domain/dadosTransacoes/TransacaoRepository.java
+++ src/main/java/com/finance/api/domain/dadosTransacoes/TransacaoRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TransacaoRepository extends JpaRepository<Transacao,Long> {
@@ -12,4 +14,6 @@ public interface TransacaoRepository extends JpaRepository<Transacao,Long> {
     Page<Transacao> findByUsuario(Usuario usuario, Pageable pageable);
 
     List<Transacao> findByUsuario(Usuario usuario);
+
+    List<Transacao> findByUsuarioAndDataBetween(Usuario usuario, LocalDateTime dataInicio, LocalDateTime dataFinal);
 }
